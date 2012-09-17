f526a79148d0d156874a7f923189f0134263e318
John Simone <john@heroku.com>
2012-09-16 21:30:45 -0700

    add support for second memcache config style, update memcache session mgr version

--- integration-tests/single_webapp/src/main/java/org/example/HelloServlet.java
+++ integration-tests/single_webapp/src/main/java/org/example/HelloServlet.java
@@ -14,7 +14,14 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
+        int count = 0;
+        if(req.getSession().getAttribute("count") != null) {
+            count = (Integer) req.getSession().getAttribute("count");
+        }
+        count++;
         out.write("hello, world".getBytes());
+        out.write(("count: " + count).getBytes());
+        req.getSession().setAttribute("count", count);
         out.flush();
         out.close();
     }
--- src/main/java/webapp/runner/launch/MemcacheSessionStore.java
+++ src/main/java/webapp/runner/launch/MemcacheSessionStore.java
@@ -13,18 +13,33 @@ class MemcacheSessionStore extends SessionStore {
      * @param ctx Tomcat context
      */
     @Override
-    public void configureSessionStore(CommandLineParams commandLineParams, Context ctx){
-        if(System.getenv("MEMCACHE_SERVERS") == null
-                || System.getenv("MEMCACHE_USERNAME") == null
-                || System.getenv("MEMCACHE_PASSWORD") == null) {
-            System.out.println("WARNING: memcache session store being used, but the required environment variables aren't set.");
-            System.out.println("Memcache session store is configured with MEMCACHE_SERVERS, MEMCACHE_USERNAME, MEMCACHE_PASSWORD");
-        }
+    public void configureSessionStore(CommandLineParams commandLineParams, Context ctx){        
         MemcachedBackupSessionManager manager = new MemcachedBackupSessionManager();
-        manager.setMemcachedNodes(System.getenv("MEMCACHE_SERVERS") + ":11211");
+        
+        if(System.getenv("MEMCACHE_SERVERS") != null) { //Using Memcache
+            if(System.getenv("MEMCACHE_USERNAME") == null
+                    || System.getenv("MEMCACHE_PASSWORD") == null) {
+                printNoConfigError();
+                return;
+            }             
+            manager.setMemcachedNodes(System.getenv("MEMCACHE_SERVERS") + ":11211");
+            manager.setUsername(System.getenv("MEMCACHE_USERNAME"));
+            manager.setPassword(System.getenv("MEMCACHE_PASSWORD"));
+        } else if(System.getenv("MEMCACHIER_SERVERS") != null) { //Using Memcachier
+            if(System.getenv("MEMCACHIER_USERNAME") == null
+                    || System.getenv("MEMCACHIER_PASSWORD") == null) {
+                printNoConfigError();
+                return;
+            }            
+            manager.setMemcachedNodes(System.getenv("MEMCACHIER_SERVERS"));
+            manager.setUsername(System.getenv("MEMCACHIER_USERNAME"));
+            manager.setPassword(System.getenv("MEMCACHIER_PASSWORD"));
+        } else {
+            printNoConfigError();
+            return;            
+        }
+        
         manager.setMemcachedProtocol("binary");
-        manager.setUsername(System.getenv("MEMCACHE_USERNAME"));
-        manager.setPassword(System.getenv("MEMCACHE_PASSWORD"));
         manager.setSticky(false);
         manager.setSessionBackupAsync(false);
         manager.setEnabled(true);
@@ -33,5 +48,11 @@ class MemcacheSessionStore extends SessionStore {
         manager.setLockingMode(commandLineParams.sessionStoreLockingMode);
         manager.setRequestUriIgnorePattern(commandLineParams.sessionStoreIgnorePattern);
         ctx.setManager(manager);
+    }
+    
+    private void printNoConfigError() {
+        System.out.println("WARNING: memcache session store being used, but the required environment variables aren't set.");
+        System.out.println("Memcache session store is configured with MEMCACHE_SERVERS, MEMCACHE_USERNAME, MEMCACHE_PASSWORD");
+        System.out.println("or MEMCACHIER_SERVERS, MEMCACHIER_USERNAME, MEMCACHIER_PASSWORD");        
     }
 }
