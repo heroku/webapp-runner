287979662c39a09a720dfc0e8cb16d088760d64d
Markus Mattinen <markus.mattinen@vincit.fi>
2014-03-24 11:36:53 +0200

    Create webapps directory in temp dir
    This fixes the following error in deployment phase:
    SEVERE: Exception fixing docBase for context []
    java.io.IOException: Application base [{1}] for host [/app/target/tomcat.5000/webapps] does not exist or is not a directory.

--- src/main/java/webapp/runner/launch/Main.java
+++ src/main/java/webapp/runner/launch/Main.java
@@ -239,6 +239,7 @@ public class Main {
      */
     static String resolveTomcatBaseDir(Integer port) throws IOException {
         final File baseDir = new File(System.getProperty("user.dir") + "/target/tomcat." + port);
+        new File(baseDir, "webapps").mkdirs();
 
         if (!baseDir.isDirectory() && !baseDir.mkdirs()) {
             throw new IOException("Could not create temp dir: " + baseDir);
