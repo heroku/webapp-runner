0bc0e7d2c7cae544024e6a181cf751eb94e561b8
Joe Kutner <jpkutner@gmail.com>
2014-12-03 08:22:54 -0600

    added a test for heroku-maven-plugin

---
+++ src/it/heroku-maven-plugin/setup.groovy
@@ -0,0 +1,13 @@
+import java.io.*;
+import org.codehaus.plexus.util.FileUtils;
+import java.util.UUID;
+
+String appName = "mvn-" + UUID.randomUUID().toString().substring(0,16);
+
+("heroku create " + appName).execute().waitFor();
+
+FileUtils.fileWrite(new File(basedir, "test.properties").getAbsolutePath(), "heroku.appName=" + appName );
+
+def process = "heroku addons:add redistogo -a${appName}".execute();
+process.waitFor();
+println(process.text)
---
+++ src/it/heroku-maven-plugin/verify.groovy
@@ -0,0 +1,45 @@
+import java.io.*;
+import org.codehaus.plexus.util.FileUtils;
+
+def props = new Properties()
+new File(basedir, "test.properties").withInputStream {
+    stream -> props.load(stream)
+}
+String appName = props["heroku.appName"]
+
+try {
+    def log = FileUtils.fileRead(new File(basedir, "build.log"));
+    if (!log.contains("BUILD SUCCESS")) {
+        throw new RuntimeException("the build was not successful")
+    }
+
+    process = "heroku config -a${appName}".execute()
+    process.waitFor()
+    output = process.text
+    assert output.contains("REDISTOGO_URL"), "The Redis add-on was not added: ${output}"
+
+    Thread.sleep(10000)
+
+    process = "heroku logs -a${appName}".execute()
+    process.waitFor()
+    output = process.text
+    assert output.contains("--expand-war"), "Did not pick up WEBAPP_RUNNER_OPTS: ${output}"
+    assert output.contains("--session-store redis"), "Did not pick up WEBAPP_RUNNER_OPTS: ${output}"
+    assert output.contains("WARNING: JedisPool not found in JNDI"), "Did not use Redis for session cache: ${output}"
+
+    process = "curl https://${appName}.herokuapp.com".execute()
+    process.waitFor()
+    output = process.text
+    if (!output.contains("hello, JSP")) {
+        throw new RuntimeException("app is not running: ${output}")
+    }
+
+    process = "curl https://${appName}.herokuapp.com/hello".execute()
+    process.waitFor()
+    output = process.text
+    if (!output.contains("hello, world")) {
+        throw new RuntimeException("servlet is not serving: ${output}")
+    }
+} finally {
+    ("heroku destroy " + appName + " --confirm " + appName).execute().waitFor();
+}
