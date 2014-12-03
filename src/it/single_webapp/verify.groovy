0bbbb65f35dcfb3f7dd5d9ea4a3105c29942c730
Joe Kutner <jpkutner@gmail.com>
2014-12-03 08:35:37 -0600

    improved test for travis

--- src/it/single_webapp/verify.groovy
+++ src/it/single_webapp/verify.groovy
@@ -6,37 +6,37 @@ def testProcess = { String command, String expectedText, String errorMessage ->
     def output = ""
     def foundText = false
     Thread.start{
-        10.times {
+        15.times {
             sleep 1000
             output = stdOut.toString() + stdErr.toString() // it ends up in StrErr for some reason
             foundText = output.contains(expectedText)
             if (foundText) process.destroy()
         }
     }
-    process.waitForOrKill(10000)
+    process.waitForOrKill(15000)
     assert foundText, "${errorMessage}: ${output}"
 }
 
 testProcess(
         "java -cp target/classes -jar ${basedir}/target/dependency/webapp-runner.jar ${basedir}/src/main/webapp",
         "Starting ProtocolHandler",
-        "The web process did not start"
+        "The web process did not start (filesystem)"
 )
 
 testProcess(
         "java -jar ${basedir}/target/dependency/webapp-runner.jar ${basedir}/target/single_webapp",
         "Starting ProtocolHandler",
-        "The web process did not start"
+        "The web process did not start (target dir)"
 )
 
 testProcess(
         "java -jar ${basedir}/target/dependency/webapp-runner.jar ${basedir}/target/single_webapp.war",
         "Starting ProtocolHandler",
-        "The web process did not start"
+        "The web process did not start (war file)"
 )
 
 testProcess(
         "java -jar ${basedir}/target/dependency/webapp-runner.jar --port 5000 ${basedir}/target/single_webapp.war",
         "Starting ProtocolHandler [\"http-nio-5000\"]",
-        "The web process did not start"
+        "The web process did not start (with port)"
 )
