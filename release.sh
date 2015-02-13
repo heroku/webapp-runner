be4e6bca8a381f87ab0b5ff90a14ef48fb493307
Joe Kutner <jpkutner@gmail.com>
2015-02-13 15:59:12 -0600

    Added some helpful notes to the release script

--- release.sh
+++ release.sh
@@ -38,3 +38,8 @@ mvn release:clean release:prepare
 # Now we can actually release (it's slightly more permantent now)
 read -p "Are you still sure you want to release? It's permanent! (press any key to continue)"
 mvn release:perform
+
+echo "Now make sure you update these articles and projects:
+
+    https://devcenter.heroku.com/articles/java-webapp-runner
+"
