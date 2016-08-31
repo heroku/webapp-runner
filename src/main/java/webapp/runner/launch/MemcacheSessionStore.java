fda1610738b04eb1b8033ec42aa4283b349ad6cc
John Garcia <g3.andrew@gmail.com>
2016-08-31 10:15:15 -0400

    add memcached cloud support to session store plugin

--- src/main/java/webapp/runner/launch/MemcacheSessionStore.java
+++ src/main/java/webapp/runner/launch/MemcacheSessionStore.java
@@ -10,14 +10,14 @@ class MemcacheSessionStore extends SessionStore {
      * Configures Memcache session manager
      *
      * @param commandLineParams Arguments map
-     * @param ctx Tomcat context
+     * @param ctx               Tomcat context
      */
     @Override
-    public void configureSessionStore(CommandLineParams commandLineParams, Context ctx){
+    public void configureSessionStore(CommandLineParams commandLineParams, Context ctx) {
         MemcachedBackupSessionManager manager = new MemcachedBackupSessionManager();
 
-        if(System.getenv("MEMCACHE_SERVERS") != null) { //Using Memcache
-            if(System.getenv("MEMCACHE_USERNAME") == null
+        if (System.getenv("MEMCACHE_SERVERS") != null) { //Using Memcache
+            if (System.getenv("MEMCACHE_USERNAME") == null
                     || System.getenv("MEMCACHE_PASSWORD") == null) {
                 printNoConfigError();
                 return;
@@ -25,25 +25,34 @@ class MemcacheSessionStore extends SessionStore {
             manager.setMemcachedNodes(System.getenv("MEMCACHE_SERVERS") + ":11211");
             manager.setUsername(System.getenv("MEMCACHE_USERNAME"));
             manager.setPassword(System.getenv("MEMCACHE_PASSWORD"));
-        } else if(System.getenv("MEMCACHIER_SERVERS") != null) { //Using Memcachier
-            if(System.getenv("MEMCACHIER_USERNAME") == null
+        } else if (System.getenv("MEMCACHIER_SERVERS") != null) { //Using Memcachier
+            if (System.getenv("MEMCACHIER_USERNAME") == null
                     || System.getenv("MEMCACHIER_PASSWORD") == null) {
                 printNoConfigError();
                 return;
             }
             String[] servers = System.getenv("MEMCACHIER_SERVERS").split(",");
             if (servers.length > 1) {
-              for (int i = 0; i < servers.length; ++i) {
-                servers[i] = "mc" + i + ":" + servers[i];
-              }
+                for (int i = 0; i < servers.length; ++i) {
+                    servers[i] = "mc" + i + ":" + servers[i];
+                }
             }
             String serversStr = servers[0];
             for (int i = 1; i < servers.length; ++i) {
-              serversStr += "," + servers[i];
+                serversStr += "," + servers[i];
             }
             manager.setMemcachedNodes(serversStr);
             manager.setUsername(System.getenv("MEMCACHIER_USERNAME"));
             manager.setPassword(System.getenv("MEMCACHIER_PASSWORD"));
+        } else if (System.getenv("MEMCACHEDCLOUD_SERVERS") != null) { //Using Memcached Cloud
+            if (System.getenv("MEMCACHEDCLOUD_USERNAME") == null
+                    || System.getenv("MEMCACHEDCLOUD_PASSWORD") == null) {
+                printNoConfigError();
+                return;
+            }
+            manager.setMemcachedNodes(System.getenv("MEMCACHEDCLOUD_SERVERS"));
+            manager.setUsername(System.getenv("MEMCACHEDCLOUD_USERNAME"));
+            manager.setPassword(System.getenv("MEMCACHEDCLOUD_PASSWORD"));
         } else {
             printNoConfigError();
             return;
