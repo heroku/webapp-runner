0bd34861031aab130ba7b1baa974f63e397ca491
Joe Kutner <jpkutner@gmail.com>
2017-12-16 13:23:31 -0600

    Cleaned up release.sh script

--- release.sh
+++ release.sh
@@ -31,12 +31,10 @@ set -e
 #   </profile>
 # </profiles>
 
-# Now we can prepare for release (it's not really permantent, but there's no harm in overreacting at this point)
+# Now we can prepare for release (it's not really that permanent, but there's no harm in overreacting at this point)
 read -p "Are you sure you want to release? It's permanent! (press any key to continue)"
 mvn release:clean release:prepare
 
-# Now we can actually release (it's slightly more permantent now)
-read -p "Are you still sure you want to release? It's permanent! (press any key to continue)"
 mvn release:perform
 
 echo "Now make sure you update these articles and projects:
