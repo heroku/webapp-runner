8dd3abf8ed3e7e04ea5a39c1ddfd571c867582a5
Joe Kutner <jpkutner@gmail.com>
2018-01-15 09:47:57 -0600

    Updating CI configuration

--- etc/travis-setup.sh
+++ etc/travis-setup.sh
@@ -1,12 +1,11 @@
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  if [ -n "`git config --get user.email`" ]; then
-    echo 'already set'; else `git config --global user.email 'buildpack@example.com'`
-  fi
-  if [ -n "`git config --get user.name`" ]; then
-    echo 'already set'; else `git config --global user.name 'BuildpackTester'`
-  fi
+#!/usr/bin/env bash
 
-  cat <<EOF > ~/.ssh/config
+[ "$CI" != "true" ] && echo "Not running on CI!" && exit 1
+
+git config --global user.email ${HEROKU_API_USER:-"buildpack@example.com"}
+git config --global user.name 'BuildpackTester'
+
+cat <<EOF >> ~/.ssh/config
 Host heroku.com
     StrictHostKeyChecking no
     CheckHostIP no
@@ -15,7 +14,16 @@ Host github.com
     StrictHostKeyChecking no
 EOF
 
-  curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
+cat <<EOF >> ~/.netrc
+machine git.heroku.com
+  login ${HEROKU_API_USER:-"buildpack@example.com"}
+  password ${HEROKU_API_KEY:-"password"}
+EOF
 
+sudo apt-get -qq update
+sudo apt-get install software-properties-common -y
+curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+if [ -n "$HEROKU_API_KEY" ]; then
   yes | heroku keys:add
 fi
