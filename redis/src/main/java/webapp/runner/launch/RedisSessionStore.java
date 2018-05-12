084cb7ad7d98e7bd0f76bf93b4123f858f711ad4
Joe Kutner <jpkutner@gmail.com>
2018-05-12 15:08:17 -0500

    Improved redis integration

--- redis/src/main/java/webapp/runner/launch/RedisSessionStore.java
+++ redis/src/main/java/webapp/runner/launch/RedisSessionStore.java
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
 
 public class RedisSessionStore extends SessionStore {
 
@@ -20,27 +21,36 @@ public class RedisSessionStore extends SessionStore {
   @Override
   public void configureSessionStore(CommandLineParams commandLineParams, Context ctx) {
 
-    String redisUri;
+    String redisUriString;
     if (System.getenv("REDIS_URL") == null && System.getenv("REDISTOGO_URL") == null && System.getenv("REDISCLOUD_URL") == null) {
-      System.out.println("WARNING: redis session store being used, but the required environment variable isn't set.");
+      System.out.println("WARNING: using redis session store, but the required environment variable isn't set.");
       System.out.println("Redis session store is configured with REDIS_URL, REDISTOGO_URL or REDISCLOUD_URL");
     } else {
       if (System.getenv("REDIS_URL") != null) {
-        redisUri = System.getenv("REDIS_URL");
+        redisUriString = System.getenv("REDIS_URL");
       } else if (System.getenv("REDISTOGO_URL") != null) {
-        redisUri = System.getenv("REDISTOGO_URL");
+        redisUriString = System.getenv("REDISTOGO_URL");
       } else {
-        redisUri = System.getenv("REDISCLOUD_URL");
+        redisUriString = System.getenv("REDISCLOUD_URL");
       }
 
+      URI redisUri = URI.create(redisUriString);
+
+      Integer poolSize = Integer.valueOf(System.getenv("REDISSON_POOL_SIZE") != null ? System.getenv("REDISSON_POOL_SIZE") : "10");
+
       Config config = new Config();
-      config.useClusterServers().addNodeAddress(redisUri);
+      config.useSingleServer()
+          .setAddress(redisUriString)
+          .setPassword(redisUri.getUserInfo().substring(redisUri.getUserInfo().indexOf(":")+1))
+          .setConnectionPoolSize(poolSize)
+          .setConnectionMinimumIdleSize(poolSize);
 
       try {
-        File temp = File.createTempFile("tempfile", ".tmp");
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(temp));) {
+        File configFile = File.createTempFile("redisson", ".json");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(configFile));) {
           bw.write(config.toJSON());
           RedissonSessionManager redisManager = new RedissonSessionManager();
+          redisManager.setConfigPath(configFile.getAbsolutePath());
           ctx.setManager(redisManager);
         }
       } catch (IOException e) {
