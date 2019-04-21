95f601008f4b99da673e489c38dfafbab9fb5157
Joe Kutner <jpkutner@gmail.com>
2019-04-21 09:49:14 -0500

    Add MAVEN_CUSTOM_OPTS to release script [skip ci]

--- release.sh
+++ release.sh
@@ -31,11 +31,11 @@ set -e
 #   </profile>
 # </profiles>
 
-./mvnw release:clean release:prepare -DdryRun
+./mvnw ${MAVEN_CUSTOM_OPTS:-} release:clean release:prepare -DdryRun
 
-./mvnw release:clean release:prepare
+./mvnw ${MAVEN_CUSTOM_OPTS:-} release:clean release:prepare
 
-./mvnw release:perform
+./mvnw ${MAVEN_CUSTOM_OPTS:-} release:perform
 
 echo "Now make sure you update these articles and projects:
 
